0d1b12bf648a100edaa5dba654fcf5ff5af1c433
Victor Neo <icyisamu@gmail.com>
2022-05-01 18:43:51 +0800

    Add Alacritty config

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ currdir=$(pwd)
 cd ~ && ln -s ${currdir}/.tmux.conf .tmux.conf
 cd ~ && ln -s ${currdir}/.vim .vim
 cd ~ && ln -s ${currdir}/.vim/.vimrc .vimrc
+cd ~ && ln -s ${currdir}/.alacritty.yml .alacritty.yml
