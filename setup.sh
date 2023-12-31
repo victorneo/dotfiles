a164476decb5fa47d33c94c9420f19279976bbc0
Victor Neo <icyisamu@gmail.com>
2023-12-31 18:53:50 +0800

    Migrate alacritty config to toml

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ cd ~ && ln -s ${currdir}/tmux.conf .tmux.conf
 cd ~ && ln -s ${currdir}/vim .vim
 cd ~ && ln -s ${currdir}/vim/.vimrc .vimrc
 cd ~ && ln -s ${currdir}/vim/.nvimrc .vimrc
-cd ~ && ln -s ${currdir}/alacritty.yml .alacritty.yml
+cd ~ && ln -s ${currdir}/alacritty.toml .alacritty.toml
 
 cd ~ && mkdir -p .config/nvim
 cd ~ && ln -s ${currdir}/nvim/init.vim .config/nvim/init.vim
