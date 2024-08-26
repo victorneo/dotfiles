836d42320028b7dfd6d9020e7d55941fc9815e8a
Victor Neo <icyisamu@gmail.com>
2024-08-26 21:13:03 +1000

    Update auto setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ cd ~ && ln -s ${currdir}/alacritty.toml .alacritty.toml
 
 cd ~ && mkdir -p .config
 cd ~ && ln -s ${currdir}/nvim .config/nvim
+cd ~ && mkdir -p ~/.config/zed
+cd ~ && ln -s ${currdir}/zed/settings.json .config/zed/settings.json
+
 
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.mnff "merge --no-ff"
